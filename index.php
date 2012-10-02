5d3f57905ac9ea45fdcbe48676821894734f32a7
John Theodore <johnted@gmail.com>
2012-10-02 18:50:09 -0400

    adding javascript

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
-<?php
-
-// Show all information, defaults to INFO_ALL
-phpinfo();
-
-// Show just the module information.
-// phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
-
-?>
+<html>
+    <head>
+        <title>Learning PHP</title>
+        <style>
+            p
+            {
+            text-align:justify;
+            font-family:Georgia;
+            }
+        </style>
+    </head>
+    <body>
+        <p>
+            <script type="text/javascript">
+                document.write("Hello World. Today is " + Date() );
+            </script>
+        </p>
+        <?php
+        
+        // Show all information, defaults to INFO_ALL
+        phpinfo();
+        
+        // Show just the module information.
+        // phpinfo(8) yields identical results.
+        phpinfo(INFO_MODULES);
+        
+        ?>
+    </body>
+</html>
